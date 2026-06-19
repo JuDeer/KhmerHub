9a81ae61908f8b77f195b84fc25b63d98a9fb1b5
JuDeer <125312003+JuDeer@users.noreply.github.com>
2026-06-19 00:00:47 -0400

    Update config.js

--- src/addons/KhmerDub/sites/config.js
+++ src/addons/KhmerDub/sites/config.js
@@ -149,8 +149,8 @@ module.exports = {
     baseUrl: ""
   },  
   english: {
-    pageSize: 300,
-    baseUrl: "https://iptv-org.github.io/iptv/index.category.m3u"
+    pageSize: 403,
+    baseUrl: "https://iptv-org.github.io/iptv/categories/sports.m3u"
   }	
   
 }; 
