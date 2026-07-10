287cd5a48f66e7c7d6a6e3c508db63ce315a161a
JuDeer <125312003+JuDeer@users.noreply.github.com>
2026-07-09 23:56:01 -0400

    Update episodes.js

--- src/addons/KhmerDub/sites/core/episodes.js
+++ src/addons/KhmerDub/sites/core/episodes.js
@@ -61,7 +61,7 @@ async function getEpisodes(prefix, seriesUrl) {
 
   const maxEp = POST_INFO.get(postId)?.maxEp || null;
 
-  let urls = [...new Set(detail.urls)];
+  let urls = detail.urls.filter(Boolean);
 
   if (maxEp && urls.length > maxEp) {
     urls = urls.slice(0, maxEp);
