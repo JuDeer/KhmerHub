0db184b12b61b9d49f0c589494fe96f8106df759
JuDeer <125312003+JuDeer@users.noreply.github.com>
2026-07-16 11:50:26 -0400

    Update manifest.js

--- src/addons/KhmerDub/manifest.js
+++ src/addons/KhmerDub/manifest.js
@@ -6,7 +6,7 @@ const sites = [
   { id: "khmertv", name: "KhmerTV", type: "channel", enabled: true }, 
   { id: "vip", name: "PhumiVip", type: "series", enabled: true },
   { id: "sunday", name: "SundayDrama", type: "series", enabled: true },
-  { id: "v4khmer", name: "Video4Khmer", type: "series", enabled: true },
+  { id: "v4khmer", name: "Video4Khmer", type: "series", enabled: false },
   { id: "phumi2", name: "PhumiClub", type: "series", enabled: true },
   { id: "khmerave", name: "KhmerAve", type: "series", enabled: true },
   { id: "merlkon", name: "Merlkon", type: "series", enabled: true },
